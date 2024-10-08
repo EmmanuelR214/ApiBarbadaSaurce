8d63eeb7be09027cf6e61b42839d1f1d4facbe04
Emmanuel <emmanuelreyes214@gmail.com>
2024-10-08 11:13:11 -0600

    add link localhost dist

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import bodyParser from 'body-parser';
 
 const app = express()
 
-const allowedOrigins = ['http://localhost:5173', 'https://labarbada.store', 'https://app.labarbada.com']
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:4173', 'https://labarbada.store', 'https://app.labarbada.com']
 app.use(cors({
     origin: allowedOrigins,
     credentials: true  
