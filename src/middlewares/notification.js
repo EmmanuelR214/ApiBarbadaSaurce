c787edb35644e7276488d3ff6c91d9e3e4bc18e8
Emmanuel <emmanuelreyes214@gmail.com>
2024-10-08 09:45:22 -0600

    correction file controller 2

--- src/middlewares/notification.js
+++ src/middlewares/notification.js
@@ -1,14 +1,21 @@
-import { Coonexion } from "../db.js"
+import { Coonexion } from "../db.js";
+
+let subscription
+
 // Ruta para recibir y almacenar la suscripción en la base de datos
 export const Subscribe = async (req, res) => {
-  const subscription = req.body;
+  subscription = req.body;
   try {    
     const endpoint = subscription.endpoint;
-    
     const p256dh = subscription.keys.p256dh;
     const auth = subscription.keys.auth;
     
-    await Coonexion.execute(`INSERT INTO suscripciones (endpoint, p256dh, auth) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE p256dh = ?, auth = ?`, [endpoint, p256dh, auth, p256dh, auth])
+    await Coonexion.execute(
+      `INSERT INTO suscripciones (endpoint, p256dh, auth) 
+      VALUES (?, ?, ?) 
+      ON DUPLICATE KEY UPDATE p256dh = ?, auth = ?`, 
+      [endpoint, p256dh, auth, p256dh, auth]
+    );    
     
     res.status(201).json(['Suscripción guardada en la base de datos.']);
   } catch (error) {
@@ -16,31 +23,3 @@ export const Subscribe = async (req, res) => {
     res.status(500).json(['Error al guardar la suscripción.', error, subscription]);
   }
 }
-
-export const Notificar = async (title, body) => {
-  try {
-    const notificacionPayload = {
-      title: title,
-      body: body,
-    }
-    const [suscripciones] = await Coonexion.query('SELECT * FROM suscripciones')
-    
-    const promises = suscripciones.map((suscripcion) =>
-      webPush.sendNotification(
-        {
-          endpoint: suscripcion.endpoint,
-          keys: {
-            p256dh: suscripcion.p256dh,
-            auth: suscripcion.auth,
-          },
-        },
-        JSON.stringify(notificacionPayload)
-      )
-    );
-    await Promise.all(promises);
-    return ['Notificaciones enviadas.'];
-  } catch (error) {
-    console.log(error)
-    return ['Error al notificar.'];
-  }
-} 
