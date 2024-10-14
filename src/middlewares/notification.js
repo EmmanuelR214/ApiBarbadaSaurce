abfd01b1523daebe81f9994d9115f92ef553a1ab
Emmanuel <emmanuelreyes214@gmail.com>
2024-10-14 01:42:10 -0600

    update token in subs

--- src/middlewares/notification.js
+++ src/middlewares/notification.js
@@ -1,20 +1,15 @@
 import { Coonexion } from "../db.js";
 
-let subscription
-
 // Ruta para recibir y almacenar la suscripción en la base de datos
 export const Subscribe = async (req, res) => {
-  subscription = req.body;
+  const {token} = req.body;
   try {    
-    const endpoint = subscription.endpoint;
-    const p256dh = subscription.keys.p256dh;
-    const auth = subscription.keys.auth;
     
     await Coonexion.execute(
-      `INSERT INTO suscripciones (endpoint, p256dh, auth) 
-      VALUES (?, ?, ?) 
-      ON DUPLICATE KEY UPDATE p256dh = ?, auth = ?`, 
-      [endpoint, p256dh, auth, p256dh, auth]
+      `INSERT INTO suscripciones (token)
+      VALUES (?) 
+      ON DUPLICATE KEY UPDATE fecha_suscripcion = CURRENT_TIMESTAMP`, 
+      [token]
     );    
     
     res.status(201).json(['Suscripción guardada en la base de datos.']);
