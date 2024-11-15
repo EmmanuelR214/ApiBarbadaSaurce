e41f240746a7c3f9e3bd411c143b7f2fbafa9efb
Emmanuel <emmanuelreyes214@gmail.com>
2024-11-15 00:33:06 -0600

    update function stripe in routes import

--- src/routers/sources.routes.js
+++ src/routers/sources.routes.js
@@ -2,7 +2,7 @@ import {Router} from 'express'
 import {validateSchema} from '../middlewares/validator.middleware.js'
 import { CargarPago, CrearVenta, DeleteCarrito, DescripcionPlatillo, getCategorias, getMenu, getMenuPorCategoria, getMenuPorNombre, GetShoppingCar, InsertShoppinCar, InteraccionWhatsApp, ObtenerDetallesXprecio, ObtenerPrecio, UpdateShoppingCar, VerificarTransaccion } from '../controllers/sources.controllers.js'
 import { Subscribe } from '../middlewares/notification.js'
-import { createPaymentStripe } from '../controllers/payment.controller.js'
+import { createPaymentIntent } from '../controllers/payment.controller.js'
 
 const router = Router()
 
@@ -40,7 +40,7 @@ router.post('/whatsapp-webhook', InteraccionWhatsApp)
 router.post('/subscribe', Subscribe)
 
 
-router.post('/create-payment-stripe', createPaymentStripe)
+router.post('/create-payment-stripe', createPaymentIntent)
 
 
 export default router
